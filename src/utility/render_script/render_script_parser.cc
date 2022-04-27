5483bce3f0db41efa8bf887d5e908c4a5f2f8bcd
QRWells <qirui.wang@moegi.waseda.jp>
2022-04-27 23:32:27 +0900

    Moved file.

--- src/utility/render_script/render_script_parser.cc
+++ src/utility/render_script/render_script_parser.cc
@@ -1,7 +1,9 @@
-#include "utility/render_script_parser.h"
+#include "utility/render_script/render_script_parser.h"
 
+#include <nlohmann/json.hpp>
+
+using namespace nlohmann;
 namespace cherry {
-auto RenderScriptLexer::Lex() -> std::vector<uint64_t> { return {}; }
 auto RenderScriptParser::Parse() -> std::shared_ptr<Renderer> const& {
   return nullptr;
 }
